808188dfeb3fff7c193039de3c6fc56d530281e2
kirantidke <kirantidke2013@.com>
2021-06-10 16:06:01 +0530

    file modified

--- lib/src/main/java/bridgelabz/SnakeLadder.java
+++ lib/src/main/java/bridgelabz/SnakeLadder.java
@@ -1,19 +1,33 @@
 package bridgelabz;
 
 public class SnakeLadder {
-
+	public static final int ladder = 1;
+	public static final int snake = 2;
+	public static final int winning_points = 100;
 	public static void main(String[] args) {
-		int player_position = 0; 
-		int random = (int)(Math.random()*6) + 1;
-		int check = (int)(Math.random()*3);
-		if ( check == 1 ) {
-			player_position += random;
-		}
-		else if ( check == 2 ) {
-			player_position -= random;
-		}
-		else 
-			player_position = player_position;
-			System.out.println(" Position of Player : " +player_position);
+		int player_position = 0;
+		int count = 1; 
+		for ( int dice = 0; player_position < winning_points; dice++ )
+		{
+			int random = (int)(Math.random()*6) + 1;
+			int check = (int)(Math.random()*3);
+			int result = 0;	
+			switch ( check ){
+				case ladder:
+					result += random;
+					break;
+				case snake:
+					result -= random;
+					break;
+				default:
+					result = 0;
+			}
+			player_position += result;
+			if ( player_position < 0 ){
+				player_position = 0;
+				}
+			count++;
+	}
+	System.out.println(" Winning Position of Player : " +player_position+ " WINNNER "+"\n"+"no. of times dice rolled "+count);
 	}
 }
