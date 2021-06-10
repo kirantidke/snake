28842c6b1c307b74786d089aa0df1e3468d08d55
kirantidke <kirantidke2013@.com>
2021-06-10 15:31:24 +0530

    file modified

--- lib/src/main/java/bridgelabz/SnakeLadder.java
+++ lib/src/main/java/bridgelabz/SnakeLadder.java
@@ -3,10 +3,17 @@ package bridgelabz;
 public class SnakeLadder {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to the simple Snake & Ladder Game");
-		int player_position = 0;
-		//System.out.println(" Starting position of player : "+player_position);
+		int player_position = 0; 
 		int random = (int)(Math.random()*6) + 1;
-		System.out.println(" Random variables from 1 to 6 : "+random);
+		int check = (int)(Math.random()*3);
+		if ( check == 1 ) {
+			player_position += random;
+		}
+		else if ( check == 2 ) {
+			player_position -= random;
+		}
+		else 
+			player_position = player_position;
+			System.out.println(" Position of Player : " +player_position);
 	}
 }
