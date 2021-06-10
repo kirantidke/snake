648a5b6b3eb6c61fad4fb6590c1c99eb34c2a4a4
kirantidke <kirantidke2013@.com>
2021-06-10 14:54:10 +0530

    file added

---
+++ lib/src/main/java/bridgelabz/SnakeLadder.java
@@ -0,0 +1,10 @@
+package bridgelabz;
+
+public class SnakeLadder {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the simple Snake & Ladder Game");
+		int player_position = 0;
+		System.out.println(" Starting position of player : "+player_position);
+	}
+}
